001e35dfc4aa348a389d40f99eb85e10436bcb66
Ndzi Nfor <ndzinfor@gmail.com>
2023-11-24 16:41:23 +0100

    Draft of GameBoard Object

--- TicTac.js
+++ TicTac.js
@@ -5,6 +5,19 @@ function Player(name, marker) {
     this.marker = marker;
   }
 
-let GameFlow = {
-
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+  
+    // Create a 2d array that will represent the state of the game board
+    // For this 2d array, row 0 will represent the top row and
+    // column 0 will represent the left-most column.
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push(Cell());
+      }
+    }
+    
 }
