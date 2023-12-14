7c2b73f69842b2572b8177cf05f9c918bd310280
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-14 18:54:01 +0100

    Complete the checkWin function

--- TicTac.js
+++ TicTac.js
@@ -24,6 +24,19 @@ const GameBoard = (() => {
         }
         return false;
     };
+    const checkWin = (player) => {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // winning rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], //winning columns
+            [0, 4, 8], [2, 4, 6], // Winning diagonals
+        ];
+        return winningCombinations.some(combination => {
+            return combination.every(index => {
+                return board[index] === player.getSymbol();
+            });
+        }); 
+    }
+
     
 
 })();
